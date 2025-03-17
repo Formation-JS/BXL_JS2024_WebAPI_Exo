1b3aa089c1c5cdae177d7209cbfdefc328feec06
Aurélien <aurelien.strimelle@bstorm.be>
2025-03-17 20:12:05 +0100

    [Member] Initial Member in "init-database"

--- src/init-database.ts
+++ src/init-database.ts
@@ -1,6 +1,10 @@
 import 'reflect-metadata';
 import { AppDataSource } from './config/db';
 import { color } from './utils/color.utils';
+import { DataSource } from 'typeorm';
+import Member, { MemberRole } from './models/member.model';
+import { createInterface } from 'readline/promises';
+import argon2 from 'argon2';
 
 //! Get params 'drop-db' in cmd
 const dropDB = process.argv.includes('--drop-db');
@@ -13,13 +17,17 @@ const dropDB = process.argv.includes('--drop-db');
     //? Sync
     const ads = await AppDataSource.initialize();
     await ads.synchronize(dropDB);
-    
+
+    if (dropDB) {
+      await initialMember(ads);
+    }
+
     console.log(color.success('Database initialize !'));
   }
-  catch(error) {
+  catch (error) {
     //? Display Error
     console.log(color.error('Database on error !'));
-    console.log(error instanceof(Error) ? error.message : error);
+    console.log(error instanceof (Error) ? error.message : error);
     exitCode = 1;
   }
   finally {
@@ -27,3 +35,38 @@ const dropDB = process.argv.includes('--drop-db');
     process.exit(exitCode);
   }
 })();
+
+async function initialMember(ads: DataSource) {
+
+  // Password for new member
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  const password = await rl.question(color.warn('Password for new members : '));
+  await rl.close();
+
+  // Initial members 
+  const memberRepo = ads.getRepository(Member);
+  const members = memberRepo.create([
+    {
+      login: 'della',
+      email: 'della.duck@demo.be',
+      firstname: 'Della',
+      lastname: 'Duck',
+      role: MemberRole.ADMIN,
+      password: await argon2.hash(password)
+    },
+    {
+      login: 'gontran',
+      email: 'gontran.bonheur@demo.be',
+      firstname: 'Gontran',
+      lastname: 'Bonheur',
+      role: MemberRole.MEMBER,
+      password: await argon2.hash(password)
+    }
+  ]);
+  await memberRepo.save(members);
+
+  console.log(color.info(`Login created for : ${members.map(m => m.login).join(', ')}.`));
+}
